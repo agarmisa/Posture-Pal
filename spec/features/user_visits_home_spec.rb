4f50bfc11efa19308cfdd332d1df3addaa7451de
agarmisa <alisongarmisa@gmail.com>
2015-10-05 15:27:41 -0400

    hound1

--- spec/features/user_visits_home_spec.rb
+++ spec/features/user_visits_home_spec.rb
@@ -6,13 +6,13 @@ feature 'user visits homepage', %{
   So that I can decide where to navigate to
 } do
   # Acceptance Criteria
-  # - User must be presented with options to log in, sign up, or visit program show page
+  # - User must be presented with options to log in or sign up
+  # - User must be presented with option to visit program show page
   # - User questionably is presented with a link to an exercise index page
 
   context 'user visits home page' do
     scenario 'user is able to see all pertinent links' do
-      program = FactoryGirl.create(:program)
-      user = FactoryGirl.create(:user)
+    FactoryGirl.create(:program)
 
       visit root_path
       expect(page).to have_content('All About the Back')
