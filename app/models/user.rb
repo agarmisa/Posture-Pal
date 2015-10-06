20319d6e265aa52e11db64233a1c586a0cfdce91
agarmisa <alisongarmisa@gmail.com>
2015-10-06 15:57:54 -0400

    hound5

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ActiveRecord::Base
 
   def self.find_or_create_from_auth_hash(auth_hash)
     User.find_or_create_by(
-    provider: auth_hash.provider,
-    uid: auth_hash.uid) do |user|
+      provider: auth_hash.provider,
+      uid: auth_hash.uid) do |user|
       user.name = auth_hash.info.name
       user.profile_pic = auth_hash.info.image
       user.email = auth_hash.info.email
