b19712a153ecc85794b902ff15b7ddffea5c4d01
agarmisa <alisongarmisa@gmail.com>
2015-10-06 15:48:11 -0400

    hound2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'],
-  ENV['FACEBOOK_SECRET'],
-  scope: 'email', info_fields: 'email, name'
+  provider :facebook,
+           ENV['FACEBOOK_KEY'],
+           ENV['FACEBOOK_SECRET'],
+           scope: 'email', info_fields: 'email, name'
 end
