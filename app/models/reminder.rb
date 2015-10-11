0d8f9fb92296a452d07462bf17cdd654331db890
agarmisa <alisongarmisa@gmail.com>
2015-10-11 14:02:04 -0400

    making reminders unique for exercise part 1

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -2,10 +2,11 @@ class Reminder < ActiveRecord::Base
   belongs_to :user
   validates :user, presence: true
 
+
   def remind(exercise)
     @twilio_number = ENV['TWILIO_NUMBER']
     @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-    reminder = "Hi #{self.user.name}. Time to exercise! #{exercise_path(exercise)}."
+    reminder = "Hi #{self.user.name}. Time to exercise! Scapular Retractions."
     message = @client.account.messages.create(
       from: @twilio_number,
       to: self.user.phone,
