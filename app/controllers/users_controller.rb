d86d438f2bc446fd6806f7513e71e4d13b577f64
agarmisa <alisongarmisa@gmail.com>
2015-10-12 22:22:13 -0400

    hound1, have duplicate update methods in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,18 +24,18 @@ class UsersController < ApplicationController
   end
 
   def update
-   @user = User.find(params[:id])
-   @user.role = "admin"
-   if @user.save
-     flash[:notice] = "User Updated"
-   end
-   redirect_to users_path
- end
+    @user = User.find(params[:id])
+    @user.role = "admin"
+    if @user.save
+      flash[:notice] = "User Updated"
+    end
+    redirect_to users_path
+  end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    flash[:notice]= "User deleted."
+    flash[:notice] = "User deleted."
     redirect_to users_path
   end
 
