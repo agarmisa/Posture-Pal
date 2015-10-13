9d9b1fb4851c3ffe20aadcb0962f9498238401ed
agarmisa <alisongarmisa@gmail.com>
2015-10-13 18:45:33 -0400

    hound2, all badges working

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -2,7 +2,12 @@ Merit.setup do |config|
 
 end
 
-badges = [{ id: 1, name: 'checked_in', custom_fields: {image: "/shooting-star.png"} }, {id: 2, name: 'checked_inx2', custom_fields: {image: "/ruby heart.png"} }]
+badges = [
+  { id: 1, name: 'checked_in',
+    custom_fields: {image: "/shooting-star.png"} },
+  { id: 2, name: 'checked_inx2',
+    custom_fields: {image: "/ruby heart.png"} }
+]
 
 badges.each do |badge|
   Merit::Badge.create!(badge)
