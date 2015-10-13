6d96ef2c727a732dd68fae1c1cc4d2f1fb23c002
agarmisa <alisongarmisa@gmail.com>
2015-10-12 20:49:17 -0400

    added basic photos to exercise show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,15 @@ Exercise.create(name: 'Scapular Retractions',
                 description: 'Draw your shoulder blades back and down, as though
                 trying to squeeze a pencil between them',
                 repetition: '3 sets of 10',
-                image: 'www.scapretract.jpg')
+                image: "https://s-media-cache-ak0.pinimg.com/736x/c9/44/96/c94496fe6381efceac3cdf8f37f52ff9.jpg")
 Exercise.create(name: 'Cervical Retraction',
                 description: 'Bring your ears backward until they line up with
                 your shoulders, think of lengthening your neck and begin to give
                 yourself a double chin, and hold for 5 seconds',
                 repetition: '1 set of 10',
-                image: 'www.cervicalretract.jpg')
+                image: 'http://www.wisconsinwellnessclinic.com/uploads/8/3/9/6/8396859/6971573_orig.jpg')
 Exercise.create(name: 'Transversus Abdominus (TA) Contraction',
                 description: 'Draw your belly button in toward your spine, keep
                 breathing, count to 5',
                 repetition: '1 set of 15',
-                image: 'www.TA.jpg')
+                image: 'http://www.massageandbodywork.com/Articles/AprilMay2005/images/hsFig7.jpg')
