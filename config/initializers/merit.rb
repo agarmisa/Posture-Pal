eeb99a557ab9d9152269644fffc20b895d42f389
agarmisa <alisongarmisa@gmail.com>
2015-10-14 16:38:54 -0400

    working wiifit badges

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -3,14 +3,22 @@ Merit.setup do |config|
 end
 
 badges = [
-  { id: 1, name: 'checked_in', description: 'Sit-Tall Squirrel',
-    custom_fields: { image: "/squirrel.jpeg" } },
+  { id: 1, name: 'checked_in', description: 'heyyyy', custom_fields: { image: "/trainer1.png" } },
+
   { id: 2, name: 'checked_inx2', description: 'Lithe Leopard',
-    custom_fields: { image: "/leopard.jpeg" } },
+    custom_fields: { image: "/trainer2.png" } },
   { id: 3, name: 'checked_inx3', description: 'Good Posture Dog',
-    custom_fields: { image: "/doggy.jpeg" } },
+    custom_fields: { image: "/trainer3.png" } },
   { id: 4, name: 'checked_inx4', description: 'Flamingo!',
-    custom_fields: { image: "/flamingo.jpeg" } }
+    custom_fields: { image: "/trainer4.png" } },
+  { id: 5, name: 'checked_inx5', description: 'Flamingo!',
+    custom_fields: { image: "/trainer5.png" } },
+  { id: 6, name: 'checked_inx5', description: 'Flamingo!',
+    custom_fields: { image: "/trainer6.png" } },
+  { id: 7, name: 'checked_inx5', description: 'Flamingo!',
+    custom_fields: { image: "/trainer7.png" } },
+  { id: 8, name: 'checked_inx5', description: 'Flamingo!',
+    custom_fields: { image: "/trainer8.png" } },
 ]
 
 badges.each do |badge|
