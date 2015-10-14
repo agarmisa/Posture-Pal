9db8baa1afd4d254b7643de371996f11faea5ac9
agarmisa <alisongarmisa@gmail.com>
2015-10-13 21:41:34 -0400

    new badge system

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -4,9 +4,13 @@ end
 
 badges = [
   { id: 1, name: 'checked_in',
-    custom_fields: { image: "/shooting-star.png" } },
+    custom_fields: { image: "/squirrel.jpeg" } },
   { id: 2, name: 'checked_inx2',
-    custom_fields: { image: "/ruby heart.png" } }
+    custom_fields: { image: "/leopard.jpeg" } },
+  { id: 3, name: 'checked_inx3',
+    custom_fields: { image: "/doggy.jpeg" } },
+  { id: 4, name: 'checked_inx4',
+    custom_fields: { image: "/flamingo.jpeg" } }
 ]
 
 badges.each do |badge|
