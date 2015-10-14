0df2fff836fbec34299bd2325beeffff346a6506
agarmisa <alisongarmisa@gmail.com>
2015-10-14 11:31:34 -0400

    some small styling

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -3,13 +3,13 @@ Merit.setup do |config|
 end
 
 badges = [
-  { id: 1, name: 'checked_in',
+  { id: 1, name: 'checked_in', description: 'Sit-Tall Squirrel',
     custom_fields: { image: "/squirrel.jpeg" } },
-  { id: 2, name: 'checked_inx2',
+  { id: 2, name: 'checked_inx2', description: 'Lithe Leopard',
     custom_fields: { image: "/leopard.jpeg" } },
-  { id: 3, name: 'checked_inx3',
+  { id: 3, name: 'checked_inx3', description: 'Good Posture Dog',
     custom_fields: { image: "/doggy.jpeg" } },
-  { id: 4, name: 'checked_inx4',
+  { id: 4, name: 'checked_inx4', description: 'Flamingo!',
     custom_fields: { image: "/flamingo.jpeg" } }
 ]
 
