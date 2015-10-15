189b63ff57f56d5e99fd8b1864305b162b960972
agarmisa <alisongarmisa@gmail.com>
2015-10-14 21:48:07 -0400

    hound1

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -3,21 +3,27 @@ Merit.setup do |config|
 end
 
 badges = [
-  { id: 1, name: 'checked_in', description: 'heyyyy', custom_fields: { image: "/trainer1.png" } },
+  { id: 1, name: 'checked_in', description: 'first trainer', custom_fields: { image: "/trainer1.png" } },
 
-  { id: 2, name: 'checked_inx2', description: 'Lithe Leopard',
+  { id: 2, name: 'checked_inx2', description: 'second trainer',
     custom_fields: { image: "/trainer2.png" } },
-  { id: 3, name: 'checked_inx3', description: 'Good Posture Dog',
+
+  { id: 3, name: 'checked_inx3', description: 'third trainer',
     custom_fields: { image: "/trainer3.png" } },
-  { id: 4, name: 'checked_inx4', description: 'Flamingo!',
+
+  { id: 4, name: 'checked_inx4', description: 'fourth trainer',
     custom_fields: { image: "/trainer4.png" } },
-  { id: 5, name: 'checked_inx5', description: 'Flamingo!',
+
+  { id: 5, name: 'checked_inx5', description: 'fifth trainer',
     custom_fields: { image: "/trainer5.png" } },
-  { id: 6, name: 'checked_inx5', description: 'Flamingo!',
+
+  { id: 6, name: 'checked_inx6', description: 'sixth trainer',
     custom_fields: { image: "/trainer6.png" } },
-  { id: 7, name: 'checked_inx5', description: 'Flamingo!',
+
+  { id: 7, name: 'checked_inx7', description: 'seventh trainer',
     custom_fields: { image: "/trainer7.png" } },
-  { id: 8, name: 'checked_inx5', description: 'Flamingo!',
+
+  { id: 8, name: 'checked_inx8', description: 'eigth trainer',
     custom_fields: { image: "/trainer8.png" } },
 ]
 
