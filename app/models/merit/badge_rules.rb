d8a2e922b3c5640bebd82db70ecae6515451e3ba
agarmisa <alisongarmisa@gmail.com>
2015-10-15 18:05:19 -0400

    hound1

--- app/models/merit/badge_rules.rb
+++ app/models/merit/badge_rules.rb
@@ -4,31 +4,31 @@ module Merit
 
     def initialize
       grant_on 'checkins#create', badge_id: 1, to: :user do |checkin|
-        checkin.count >= 5
-      end
-
-      grant_on 'checkins#create', badge_id: 2, to: :user do |checkin|
         checkin.count >= 10
       end
 
-      grant_on 'checkins#create', badge_id: 3, to: :user do |checkin|
-        checkin.count >= 15
-      end
-
-      grant_on 'checkins#create', badge_id: 4, to: :user do |checkin|
-        checkin.count >= 20
-      end
-
-      grant_on 'checkins#create', badge_id: 5, to: :user do |checkin|
+      grant_on 'checkins#create', badge_id: 2, to: :user do |checkin|
         checkin.count >= 25
       end
 
+      grant_on 'checkins#create', badge_id: 3, to: :user do |checkin|
+        checkin.count >= 50
+      end
+
+      grant_on 'checkins#create', badge_id: 4, to: :user do |checkin|
+        checkin.count >= 75
+      end
+
+      grant_on 'checkins#create', badge_id: 5, to: :user do |checkin|
+        checkin.count >= 100
+      end
+
       grant_on 'checkins#create', badge_id: 6, to: :user do |checkin|
-        checkin.count >= 30
+        checkin.count >= 200
       end
 
       grant_on 'checkins#create', badge_id: 7, to: :user do |checkin|
-        checkin.count >= 35
+        checkin.count >= 300
       end
 
       grant_on 'checkins#create', badge_id: 8, to: :user do |checkin|
