adb909faacf0ec003430e888d30b4a85822a1394
agarmisa <alisongarmisa@gmail.com>
2015-10-15 17:34:25 -0400

    better user show page

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -3,28 +3,28 @@ Merit.setup do |config|
 end
 
 badges = [
-  { id: 1, name: 'checked_in', description: 'first trainer', custom_fields: { image: "/trainer1.png" } },
+  { id: 1, name: 'checked_in', description: 'first trainer', custom_fields: { image: "/Icon Star.png" } },
 
   { id: 2, name: 'checked_inx2', description: 'second trainer',
-    custom_fields: { image: "/trainer2.png" } },
+    custom_fields: { image: "/Icon Star.png" } },
 
   { id: 3, name: 'checked_inx3', description: 'third trainer',
-    custom_fields: { image: "/trainer3.png" } },
+    custom_fields: { image: "/Icon Star.png" } },
 
   { id: 4, name: 'checked_inx4', description: 'fourth trainer',
-    custom_fields: { image: "/trainer4.png" } },
+    custom_fields: { image: "/Icon Star.png" } },
 
   { id: 5, name: 'checked_inx5', description: 'fifth trainer',
-    custom_fields: { image: "/trainer5.png" } },
+    custom_fields: { image: "/Icon Star.png" } },
 
   { id: 6, name: 'checked_inx6', description: 'sixth trainer',
-    custom_fields: { image: "/trainer6.png" } },
+    custom_fields: { image: "/Icon Star.png" } },
 
   { id: 7, name: 'checked_inx7', description: 'seventh trainer',
-    custom_fields: { image: "/trainer7.png" } },
+    custom_fields: { image: "/Icon Star.png" } },
 
   { id: 8, name: 'checked_inx8', description: 'eigth trainer',
-    custom_fields: { image: "/trainer8.png" } },
+    custom_fields: { image: "/Icon Star.png" } },
 ]
 
 badges.each do |badge|
