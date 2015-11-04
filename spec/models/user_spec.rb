1135bac1e7b9b2cc135d1e53a839d711a950c7e0
agarmisa <alisongarmisa@gmail.com>
2015-11-03 21:24:51 -0500

    hound1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it { should have_valid(:email).when('PorkyPig@pigmail.com') }
+  it { should have_valid(:email).when('PorkyPig@pigmail.com') }
 end
